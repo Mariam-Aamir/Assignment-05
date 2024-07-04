793ece32683823f5731a13181aff9c33909d0772
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-04 16:20:58 +0500

    fifth  assignment

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-//Assignment 04
-
-//storing famous Quote and Author name in variabe
-
 let famousQuote :string ="Nations are born in the hearts of poets, they prosper and die in the hands of politicians"
 
 let famousAuthor :string = "Allama Iqbal"
