5993582011a4c10d6e9fda0018ee9ead8e52abd4
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-04 15:50:25 +0500

    fifth  assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Assignment 05
+//storing famous Quote and Author name in variabe
+let famousQuote = "Nations are born in the hearts of poets, they prosper and die in the hands of politicians";
+let famousAuthor = "Allama Iqbal";
+console.log(`${famousAuthor} Once said, "${famousQuote}" `);
---
+++ main.ts
@@ -0,0 +1,21 @@
+//Assignment 05
+
+//storing famous Quote and Author name in variabe
+
+let famousQuote :string ="Nations are born in the hearts of poets, they prosper and die in the hands of politicians"
+
+let famousAuthor :string = "Allama Iqbal"
+
+console.log(`${famousAuthor} Once said, "${famousQuote}" `)
+
+
+
+
+
+
+
+
+
+
+
+
